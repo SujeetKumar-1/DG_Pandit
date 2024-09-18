527fa85c12a7e0d2d1ae8c4d527f81b6c819d8d9
Sujeet Kumar <130788061+SujeetKumar-1@users.noreply.github.com>
2024-09-18 17:02:17 +0530

    Update settings.py

--- DG_Pandit/settings.py
+++ DG_Pandit/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sujeetkumar01357@gmail.com'
-EMAIL_HOST_PASSWORD = 'nraa djlz lkpg kqvu'
+EMAIL_HOST_USER = 'yourmial@gmail.com'
+EMAIL_HOST_PASSWORD = 'nraa djlz lkpg'
 
 
 
